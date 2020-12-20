acf156e150b41ec020c7601a390aea650a5ceebd
Broly190 <cad4464fd7atyka@yahoo.ne.jp>
2020-12-20 14:30:15 +0900

    メッセージ送信機能の実装

--- Gemfile
+++ Gemfile
@@ -59,3 +59,7 @@ gem 'font-awesome-sass'
 gem 'devise'
 
 gem 'pry-rails'
+
+gem 'carrierwave'
+
+gem 'mini_magick'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "groups#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
